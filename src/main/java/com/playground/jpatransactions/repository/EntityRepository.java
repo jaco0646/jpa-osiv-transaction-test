9e2bfcb24a0dea01b3a45624332ea9015e11372b
jaco0646 <1025949+jaco0646@users.noreply.github.com>
2023-10-26 19:47:44 -0500

    test query for dynamic projection

--- src/main/java/com/playground/jpatransactions/repository/EntityRepository.java
+++ src/main/java/com/playground/jpatransactions/repository/EntityRepository.java
@@ -3,6 +3,7 @@ package com.playground.jpatransactions.repository;
 import com.playground.jpatransactions.data.SuperInterfaceDTO;
 import com.playground.jpatransactions.data.MyEntity;
 import com.playground.jpatransactions.data.UnrelatedInterfaceDTO;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,9 @@ public interface EntityRepository extends CrudRepository<MyEntity, Integer> {
 
     @Query(nativeQuery = true, value = "select * from entity where id = :id")
     UnrelatedInterfaceDTO findUnrelatedInterfaceById(int id);
+
+    @Query(nativeQuery = true, value = "select * from entity where id = :id")
+    Tuple findTupleById(int id);
+
+    <T> T findById(int id, Class<T> dto);
 }
--- src/test/groovy/com/playground/jpatransactions/repository/EntityRepositorySpec.groovy
+++ src/test/groovy/com/playground/jpatransactions/repository/EntityRepositorySpec.groovy
@@ -1,6 +1,8 @@
 package com.playground.jpatransactions.repository
 
 import com.playground.jpatransactions.data.MyEntity
+import com.playground.jpatransactions.data.SuperInterfaceDTO
+import com.playground.jpatransactions.data.UnrelatedInterfaceDTO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.core.convert.ConversionFailedException
@@ -19,7 +21,7 @@ class EntityRepositorySpec extends Specification {
             repo1.viewByID(1).getNumberOne()
     }
 
-    def "test when MyEntity does not implement the DTO"() {
+    def "test Native Query Projection - unrelated interface"() {
         given:
             def dto = repo.findUnrelatedInterfaceById(1)
         expect:
@@ -27,11 +29,34 @@ class EntityRepositorySpec extends Specification {
             dto.getFoo()
     }
 
-    def "test when MyEntity implements the DTO"() {
+    def "test Native Query Projection - super interface"() {
         when:
             def dto = repo.findSuperInterfaceById(1)
         then:
             def e = thrown(ConversionFailedException)
             e.message.contains('Failed to convert from type [java.lang.Object[]]')
+    }
+
+    def "test Dynamic Projection - unrelated interface"() {
+        given:
+            def dto = repo.findById(1, UnrelatedInterfaceDTO)
+        expect:
+            dto.getClass() != MyEntity
+            dto.getFoo()
+    }
+
+    def "test Dynamic Projection - super interface"() {
+        given:
+            def dto = repo.findById(1, SuperInterfaceDTO)
+        expect:
+            dto.getClass() == MyEntity
+            dto.getFoo()
+    }
+
+    def "test Tuple Projection"() {
+        given:
+            def dto = repo.findTupleById(1)
+        expect:
+            dto.get('foo')
     }
 }
